4ea3662138cc45c2b9f1613f72b3ad9d48dbac9f
imunellka <107074158+imunellka@users.noreply.github.com>
2023-03-26 15:40:27 +0300

    beautify

--- src/main/java/ru/hse/agent/AgentRepository.java
+++ src/main/java/ru/hse/agent/AgentRepository.java
@@ -1,11 +1,11 @@
 package ru.hse.agent;
 
-import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
-
 import lombok.Getter;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * Класс для доступа ко всем агентам
@@ -41,44 +41,44 @@ public class AgentRepository {
         log.info("Agent removed from repository: {}", agent.getName());
     }
 
-  /**
-   * Находит всех агентов искомого типа
-   *
-   * @param type тип искомого агента
-   * @return список всех рабочих агентов
-   */
-  @SuppressWarnings("unchecked")
-  public static synchronized <T extends Agent<?>> List<T> findByType(Class<T> type) {
-    return AGENTS.stream()
-        .filter(agent -> agent.getClass() == type)
-        .map(agent -> (T) agent)
-        .toList();
-  }
+    /**
+     * Находит всех агентов искомого типа
+     *
+     * @param type тип искомого агента
+     * @return список всех рабочих агентов
+     */
+    @SuppressWarnings("unchecked")
+    public static synchronized <T extends Agent<?>> List<T> findByType(Class<T> type) {
+        return AGENTS.stream()
+                .filter(agent -> agent.getClass() == type)
+                .map(agent -> (T) agent)
+                .toList();
+    }
 
-  /**
-   * Проверяем, есть ли агент в репозитории
-   *
-   * @param type Агент
-   * @param id Айди
-   * @return true, если агент найден; иначе – false
-   * @param <T> Класс, наследуемый от агента
-   */
-  public static synchronized <T extends Agent<?>> boolean isAgentExist(Class<T> type, int id) {
-    return AGENTS.stream().anyMatch(agent -> agent.getId() == id && agent.getClass() == type);
-  }
+    /**
+     * Проверяем, есть ли агент в репозитории
+     *
+     * @param type Агент
+     * @param id   Айди
+     * @param <T>  Класс, наследуемый от агента
+     * @return true, если агент найден; иначе – false
+     */
+    public static synchronized <T extends Agent<?>> boolean isAgentExist(Class<T> type, int id) {
+        return AGENTS.stream().anyMatch(agent -> agent.getId() == id && agent.getClass() == type);
+    }
 
-  /**
-   * Ищем нужного агента в репозитории по классу и айди
-   *
-   * @param type Агент
-   * @param id Айди агента
-   * @return найденный агент в репозитории или null;
-   * @param <T> Класс, наследуемый от агента
-   */
-  public static <T extends Agent<?>> Agent<?> findByTypeAndId(Class<T> type, int id) {
-    return AGENTS.stream()
-        .filter(agent -> agent.getId() == id && agent.getClass() == type)
-        .findFirst()
-        .orElse(null);
-  }
+    /**
+     * Ищем нужного агента в репозитории по классу и айди
+     *
+     * @param type Агент
+     * @param id   Айди агента
+     * @param <T>  Класс, наследуемый от агента
+     * @return найденный агент в репозитории или null;
+     */
+    public static <T extends Agent<?>> Agent<?> findByTypeAndId(Class<T> type, int id) {
+        return AGENTS.stream()
+                .filter(agent -> agent.getId() == id && agent.getClass() == type)
+                .findFirst()
+                .orElse(null);
+    }
 }

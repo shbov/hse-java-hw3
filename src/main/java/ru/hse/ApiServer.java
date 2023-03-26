de3990cb43128c5c9abdc4d0f05e7535efcd33e1
Andrew Shubnikov <a.shbov@yandex.ru>
2023-03-26 13:56:55 +0300

    add api

--- src/main/java/ru/hse/ApiServer.java
+++ src/main/java/ru/hse/ApiServer.java
@@ -9,10 +9,13 @@ import java.util.List;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.IOUtils;
+import ru.hse.agent.AgentRepository;
 import ru.hse.agent.Visitor;
 import ru.hse.message.Message;
 import ru.hse.message.visitor.RequestMenuOut;
 import ru.hse.utilities.DeserializeUtility;
+import spark.Request;
+import spark.Response;
 
 @Slf4j
 @UtilityClass
@@ -21,56 +24,63 @@ public class ApiServer {
     path(
         "/api",
         () -> {
-          post(
-              "/visitor-order",
-              "application/json",
-              (req, res) -> {
-                Visitor visitor = null;
-
-                try {
-                  visitor =
-                      DeserializeUtility.deserializeObject(
-                          req.queryParams("visitor"), new TypeReference<>() {});
-                } catch (Exception e) {
-                  log.error(e.getMessage());
-                }
-
-                if (visitor != null) {
-                  log.info("Пришел пользователь из API: {}", visitor);
-                  Visitor.start(visitor);
-
-                  Message mess = new RequestMenuOut(visitor.getId());
-                  Main.getSuperVisor().registerMessage(mess);
-
-                  Thread.sleep(100);
-                  return "заказ получен и находится в работе";
-                }
-
-                return "error: request is invalid";
-              });
-          get(
-              "/logs",
-              (req, res) -> {
-                String result = "{}";
-
-                try {
-                  FileInputStream fis = new FileInputStream("storage/logs/log.json");
-                  List<String> json = IOUtils.readLines(fis);
-
-                  if (!json.isEmpty()) {
-                    result = "[" + joinLinesWithComma(json) + "]";
-                  } else {
-                    result = json.toString();
-                  }
-                } catch (IOException exception) {
-                  log.error(exception.toString());
-                }
-
-                res.type("application/json\n");
-                res.status(200);
-                return result;
-              });
+          post("/visitor-order", "application/json", ApiServer::VisitorOrderMethod);
+          get("/logs", ApiServer::getLogsMethod);
         });
+  }
+
+  private static String getLogsMethod(Request req, Response res) {
+    String result = "{}";
+
+    try {
+      FileInputStream fis = new FileInputStream("storage/logs/log.json");
+      List<String> json = IOUtils.readLines(fis);
+
+      if (!json.isEmpty()) {
+        result = "[" + joinLinesWithComma(json) + "]";
+      } else {
+        result = json.toString();
+      }
+    } catch (IOException exception) {
+      log.error(exception.toString());
+    }
+
+    res.type("application/json");
+    res.status(200);
+    return result;
+  }
+
+  private static String VisitorOrderMethod(Request req, Response res) {
+    res.type("application/json");
+    Visitor visitor = null;
+
+    try {
+      visitor =
+          DeserializeUtility.deserializeObject(
+              req.queryParams("visitor"), new TypeReference<>() {});
+    } catch (Exception e) {
+      log.error(e.getMessage());
+    }
+
+    if (visitor != null) {
+      log.info("Пришел пользователь из API: {}", visitor);
+      if (AgentRepository.isAgentExist(Visitor.class, visitor.getId())) {
+        res.status(400);
+        return "{\"message\": \"visitor with id="
+            + visitor.getId()
+            + " already exists!\", \"status\": \"error\"}";
+      }
+
+      Visitor.start(visitor);
+      Message mess = new RequestMenuOut(visitor.getId());
+      Main.getSuperVisor().registerMessage(mess);
+      res.status(200);
+
+      return "{\"message\": \"Спасибо за заказ! Он уже в работе\", \"status\": \"success\"}";
+    }
+
+    res.status(400);
+    return "{\"message\": \"request is invalid\", \"status\": \"error\"}";
   }
 
   private static String joinLinesWithComma(List<String> lines) {
